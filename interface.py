658440cb2029ef02b68d2ab52cd0f9cbb030ef4c
blacknand <nblackburndeveloper@icloud.com>
2024-06-24 15:10:25 +0100

    add temp into gui

--- interface.py
+++ interface.py
@@ -2,21 +2,22 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from time_data import formatted_cur_date, current_time
 from bme280 import bme280_results
 
 class BME280App(App):
-    def __init__(self, bme_results):
-        self.bme280_results = bme_results
-        self.bme_temp = bme280_results[0]
-        self.bme_humidity = bme280_results[1]
-        self.bme_pressure = bme280_results[2]
-        self.bme_altitude = bme280_results[3]
-        self.bme_dew_point = bme280_results[-1]
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        bme_results = bme280_results()
+        self.bme_temp = bme_results[0]
+        self.bme_humidity = bme_results[1]
+        self.bme_pressure = bme_results[2]
+        self.bme_altitude = bme_results[3]
+        self.bme_dew_point = bme_results[-1]
 
     def build(self):
-        label = Label(text=self.bme_temp)
+        label = Label(text=str(self.bme_temp))
+        return label
 
     
 if __name__ == "__main__":
-    BuildApp(bme280_results()).run()
+    BME280App().run()
