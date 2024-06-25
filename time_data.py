56cc3a7a174609cdcc5e8d56c575903dccbf2420
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-06-25 21:51:00 +0100

    fix f string quote error

--- time_data.py
+++ time_data.py
@@ -2,7 +2,7 @@ from datetime import datetime, date
 from time import localtime
 
 today = date.today()
-formatted_cur_date = f"{today.strftime("%a")}, {today.strftime("%b")} {today.strftime("%d")}"
+formatted_cur_date = f'{today.strftime("%a")}, {today.strftime("%b")} {today.strftime("%d")}'
 current_time = datetime.now().strftime("%H:%M:%S")
 
 print(formatted_cur_date, current_time)
