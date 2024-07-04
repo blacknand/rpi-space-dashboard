a7dffd145f45655238835a56b8ce93a9d17291ba
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-07-04 21:09:09 +0100

    change launches into class add JSON file dump method

--- interface.py
+++ interface.py
@@ -8,48 +8,4 @@ from bme280 import bme280_results
 from kivy.core.window import Window
 from time_data import current_time, formatted_cur_date
 
-class BME280App(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        bme_results = bme280_results()
-        self.bme_temp = f"TMP: {bme_results[0]}"
-        self.bme_humidity = f"RM: {bme_results[1]}"
-        self.bme_pressure = f"Pb: {bme_results[2]}"
-        self.bme_dew_point = f"DWPNT: {bme_results[-1]}"
-        self.bme_temp_label = Label(text=str(self.bme_temp))
-        self.bme_humidity_label = Label(text=str(self.bme_humidity))
-        self.current_time_label = Label(text=str(current_time))
-        self.formatted_cur_date_label = Label(text=str(formatted_cur_date))
-        self.bme_pressure_label = Label(text=str(self.bme_pressure))
-        self.bme_dewpoint_label = Label(text=str(self.bme_dew_point))
-
-    def build(self):
-        Window.show_cursor = False
-        layout = BoxLayout(orientation='horizontal')
-        layout.add_widget(self.bme_temp_label)
-        layout.add_widget(self.bme_humidity_label)
-        layout.add_widget(self.current_time_label)
-        layout.add_widget(self.current_time_label)
-        layout.add_widget(self.bme_pressure_label)
-        layout.add_widget(self.bme_dewpoint_label)
-        Clock.schedule_interval(self.update_bme_data, 1)
-        return layout
-    
-    def update_bme_data(self, dt):
-        bme_results = bme280_results()
-        self.bme_temp = f"TMP: {bme_results[0]}"
-        self.bme_humidity = f"RM: {bme_results[1]}"
-        self.current_time_label = Label(text=str(current_time))
-        self.formatted_cur_date_label = Label(text=str(formatted_cur_date))
-        self.bme_pressure = f"Pb: {bme_results[2]}"
-        self.bme_dew_point = f"DWPNT: {bme_results[-1]}"
-        self.bme_temp_label.text = str(self.bme_temp)
-        self.bme_humidity_label = str(self.bme_humidity)
-        self.current_time_label = str(self.current_time_label)
-        self.formatted_cur_date_label = str(self.formatted_cur_date_label)
-        self.bme_pressure_label = str(self.bme_pressure)
-        self.bme_dewpoint_label = str(self.bme_dew_point)
-
-    
-if __name__ == "__main__":
-    BME280App().run()
+# TODO: Just the GUI
--- rocket_launches.py
+++ rocket_launches.py
@@ -1,34 +1,47 @@
 import requests
 import json
-import requests
-from datetime import datetime, timedelta
 
-launch_base_url = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
-# upcoming_launch_request = requests.get(f"{launch_base_url}upcoming/")
+class RocketLaunchesData:
+    def __init__(self, query_url: str):
+        self.query_url = query_url
+        self.query_results = None                                           # Initial state
+
+    def rocket_query_results(self) -> dict or None:
+        try:
+            query_results = requests.get(self.query_url)
+            query_results.raise_for_status()
+        except Exception as e:
+            print(f"RocketLaunchesData::rocket_query_results: Exception has occured: {e}")
+            return None
+        else:
+            self.query_results = query_results.json()
+            return self.query_results
+        
+    def json_file_dump(self, file: str) -> None:
+        self.query_results = self.rocket_query_results()
+        if self.query_results is None:
+            raise Exception(f"Error writing to {file}. No launch data")
+
+        try:
+            with open(file, 'w', encoding='utf-8') as f:
+                json.dump(self.query_results, f, ensure_ascii=False, indent=4)
+        except Exception as e:
+            print(f"RocketLaunchesData::json_file_dump: Exception has occured: {e}")
 
 
-filters = "limit=10&include_suborbital=true&hide_recent_previous=true&ordering=net&mode=list"
-# test_filters = "hide_recent_previous=true&limit=10"
-test_url = f"{launch_base_url}?{filters}"
-# print(f"Test URL: {test_url}")
+# launch_base_url = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
+# filters = "limit=25&include_suborbital=true&hide_recent_previous=true&ordering=net&mode=list&tbd=true"
+# test_url = f"{launch_base_url}?{filters}"
 
-def rocket_query_results(query_url: str) -> dict or None:
-    try:
-        query_results = requests.get(query_url)
-    except Exception as e:
-        print(f"Exception has occured: {e}")
-    else:
-        query_status = query_results.status_code
-        print(f"Response status code: {query_status}")
-        if query_status != 200:
-            return
-        return query_results.json()
-    
-query_results = rocket_query_results(test_url)
-if not query_results:
-    quit()
 
-for result in query_results["results"]:
-    # print(f"{result["name"]}\n\n")
-    print(f"{result}\n\n\n\n\n")
+# query_results = rocket_query_results(test_url)
+# if not query_results:
+#     quit()                                          # TODO: Thorough error handling
+
+# with open('ll2data.json', 'w', encoding='utf-8') as f:
+#     json.dump(query_results, f, ensure_ascii=False, indent=4)
+
+# for result in query_results["results"]:
+#     # print(f"{result["name"]}\n\n")
+#     print(f"{result}\n\n\n\n\n")
 
