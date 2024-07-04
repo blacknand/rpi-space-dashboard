b23198afb55b6ab79f0185cd3b23efad4d11476b
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-07-04 22:46:47 +0100

    added in unfinished helper methods

--- rocket_launches.py
+++ rocket_launches.py
@@ -1,5 +1,11 @@
+"""
+Uses the LL2 API to get upcoming rocket launches
+https://ll.thespacedevs.com/2.2.0/swagger/#/launch/launch_upcoming_list
+"""
+
 import requests
 import json
+import sys
 
 class RocketLaunchesData:
     def __init__(self, query_url: str):
@@ -28,10 +34,31 @@ class RocketLaunchesData:
         except Exception as e:
             print(f"RocketLaunchesData::json_file_dump: Exception has occured: {e}")
 
+    def get_filtered_results(self) -> json:
+        # filtered_data = {launch_key: self.query_results["results"]} 
+        pass
 
-# launch_base_url = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
-# filters = "limit=25&include_suborbital=true&hide_recent_previous=true&ordering=net&mode=list&tbd=true"
-# test_url = f"{launch_base_url}?{filters}"
+    def json_test_filter(self, json_file: str) ->json:
+        open_json_file = open(json_file)
+        json_data = json.load(open_json_file)
+        json_data = json.dumps(json_data)                                       # Convert to dict to handle null values
+        return {self.catch_json_error(lambda: json_data["results"][key]) for key in json_data["results"]}
+
+
+    # https://stackoverflow.com/questions/27140090/how-can-json-data-with-null-value-be-converted-to-a-dictionary
+    def catch_json_error(func, *args, handle=lambda e : e, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            return handle(e)
+
+launch_base_url = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming/"
+filters = "limit=25&include_suborbital=true&hide_recent_previous=true&ordering=net&mode=list&tbd=true"
+test_url = f"{launch_base_url}?{filters}"
+
+test_obj = RocketLaunchesData(test_url)
+test_results = test_obj.json_test_filter(sys.argv[1])
+# print(test_obj.catch_json_error(test_results))
 
 
 # query_results = rocket_query_results(test_url)
@@ -45,3 +72,4 @@ class RocketLaunchesData:
 #     # print(f"{result["name"]}\n\n")
 #     print(f"{result}\n\n\n\n\n")
 
+print(sys.argv[1])
