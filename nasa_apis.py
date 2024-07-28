a4819854b27d76415f012c73dfc0fe56907f2680
blacknand <nblackburndeveloper@icloud.com>
2024-07-28 16:13:34 +0100

    add nasa_apis.py

--- nasa_apis.py
+++ nasa_apis.py
@@ -3,12 +3,9 @@ import requests
 import json
 import os
 
-load_dotenv(".env")
-NASA_API_KEY = os.environ.get("NASA_API_KEY")
+class NASAAPis:
+    def __init__(self):
+        pass
 
-INSIGHT_API_URL = f"https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
-
-insight_api_results = requests.get(INSIGHT_API_URL)
-insight_api_json = insight_api_results.json()
-
-print(insight_api_json)
+    def apod_resonse(self):
+        pass
