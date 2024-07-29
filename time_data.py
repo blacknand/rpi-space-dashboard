2b740c9a1ee70b87a938bd51aff6e64e4771dcab
Nathan Blackburn <nblackburndeveloper@icloud.com>
2024-07-29 21:32:50 +0100

    rm un-needed files

--- time_data.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime, date
-from time import localtime
-
-today = date.today()
-formatted_cur_date = f'{today.strftime("%a")}, {today.strftime("%b")} {today.strftime("%d")}'
-current_time = datetime.now().strftime("%H:%M:%S")
