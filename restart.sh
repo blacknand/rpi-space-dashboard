71fe7ef89e7511d15e85940b138822b3ee93a6cd
blacknand <nblackburndeveloper@icloud.com>
2024-09-20 00:11:40 +0100

    remove shell script in replacement of <interface.py> doing the restart automatically

--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-pkill -f /home/blacknand/programming/rpi-space-dashboard-new/interface.py
-source /home/blacknand/programming/rpi-space-dashboard-new/env/bin/activate
-python3 /home/blacknand/programming/rpi-space-dashboard-new/interface.py
-echo "Restart script ran at $(date)" >> /home/blacknand/programming/rpi-space-dashboard-new/cron_test.log 2>&1
